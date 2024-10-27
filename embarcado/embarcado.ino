12ec152aeba94cb116c6bd52513ab748d6c88465
Lucas R. Martins <lucasr300@hotmail.com>
2024-10-27 18:55:19 -0300

    verificando temperatura e umidade atraves de um sensor dht22

---
+++ embarcado/embarcado.ino
@@ -0,0 +1,39 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#define DHTTYPE      DHT22
+#define DHTPIN 2         
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  dht.humidity().getSensor(&sensor);
+  delayMS = sensor.min_delay / 1000;
+}
+
+void loop() {
+  delay(delayMS);
+  sensors_event_t event;
+
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)){
+    Serial.println("E Erro na leitura da Temperatura!");
+  } else {
+    Serial.print("T ");
+    Serial.println(event.temperature);
+  }
+
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)){
+    Serial.println("E Erro na leitura da Umidade!");
+  }
+  else {
+    Serial.print("U ");
+    Serial.println(event.relative_humidity);
+  }
+}
